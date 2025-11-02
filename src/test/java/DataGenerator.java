7c5c82bfee25cb78177e942a78e75c6f45d3ad38
Grigory <gregkarpov64@gmail.com>
2025-11-02 12:52:26 +0400

    fix: remove api-related method from ui project

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -20,14 +20,4 @@ public class DataGenerator {
         return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    // ВОТ НОВЫЙ МЕТОД, ТЕПЕРЬ ОН ВНУТРИ КЛАССА
-    public static RegistrationDto generateRegistration(String status) {
-        Faker faker = new Faker(new Locale("en"));
-        return new RegistrationDto(
-                faker.name().username(),
-                faker.internet().password(),
-                status
-        );
-    }
-
 }
